f88ab63705996f4e545b67505235c7fc9aaaf675
Freddie Simons <freddie_simons@hotmail.co.uk>
2018-02-25 16:32:18 +0100

    adds guess component

--- src/components/Hangman.js
+++ src/components/Hangman.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import './Hangman.css'
+import {connect} from 'react-redux'
 import img0 from '../images/img0.jpg'
 import img1 from '../images/img1.jpg'
 import img2 from '../images/img2.jpg'
@@ -13,19 +14,43 @@ import img8 from '../images/img8.jpg'
 import img9 from '../images/img9.jpg'
 import img10 from '../images/img10.jpg'
 import img11 from '../images/img10.jpg'
+import {wrongGuesses} from '../functions'
 
 
 class Hangman extends PureComponent {
   static propTypes = {
-    value: PropTypes.number.isRequired,
+    word: PropTypes.string.isRequired,
+    guesses: PropTypes.array.isRequired
   }
 
   render() {
-    return (
-      <div className="Hangman">
-      img src={'img0.jpg'}
-      </div>
-    )
-  }
+    const images = [
+      img0,
+      img1,
+      img2,
+      img3,
+      img4,
+      img5,
+      img6,
+      img7,
+      img8,
+      img9,
+      img10,
+      img11,
+    ]
 
+    const {word, guesses} = this.props
+    		return (
+    			<img alt='Hangman' src={images[wrongGuesses(word, guesses)]} />
+    		)
+      }
+}
+
+
+const mapStateToProps = (state) => {
+    return {
+    word: state.word,
+    guesses: state.guesses
+  }
+}
 export default connect(mapStateToProps)(Hangman)
