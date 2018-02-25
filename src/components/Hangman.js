d9f2dca302c617536a9fefb960d25c6841e2270b
Freddie Simons <freddie_simons@hotmail.co.uk>
2018-02-25 14:36:40 +0100

    adds store

--- src/components/Hangman.js
+++ src/components/Hangman.js
@@ -1,7 +1,19 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import './Hangman.css'
-import img from './img0'
+import img0 from '../images/img0.jpg'
+import img1 from '../images/img1.jpg'
+import img2 from '../images/img2.jpg'
+import img3 from '../images/img3.jpg'
+import img4 from '../images/img4.jpg'
+import img5 from '../images/img5.jpg'
+import img6 from '../images/img6.jpg'
+import img7 from '../images/img7.jpg'
+import img8 from '../images/img8.jpg'
+import img9 from '../images/img9.jpg'
+import img10 from '../images/img10.jpg'
+import img11 from '../images/img10.jpg'
+
 
 class Hangman extends PureComponent {
   static propTypes = {
@@ -12,9 +24,8 @@ class Hangman extends PureComponent {
     return (
       <div className="Hangman">
       img src={'img0.jpg'}
-      <div/>
+      </div>
     )
   }
-}
-//const mapStateToProps = ({ hangman }) => ({ hangman })
-export default Hangman
+
+export default connect(mapStateToProps)(Hangman)
